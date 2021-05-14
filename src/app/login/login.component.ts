6ad195ba6d0256be3b3d1bf0df833cdda8b8d73b
mahithaa20 <mahithaarameshkumar20@gmail.com>
2021-05-14 12:09:43 +0530

    Added log for exception

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -41,6 +41,7 @@ export class LoginComponent implements OnInit {
         window.alert("Wrong Credentials");
       }
     }).catch(function(error) {
+      console.log("Exception");
       console.log(error);
     });
   }
